4bf831a0741666473d5321f91922002e3a397dd4
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-03-10 18:40:11 +0530

    Time: 4 ms (60.88%) | Memory: 64.9 MB (99.38%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_set<int>set;
-        for(int i= 0 ;i<nums.size();i++){
-           if(set.count(nums[i])) return nums[i];
-           set.insert(nums[i]);
+        //using concept of loop in linked list 
+        //fast and slow pointer 
+
+        //start a fast and slow pointer until they meet
+        int slow = 0 , fast = 0 ;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            } while (slow!=fast);
+
+        //as soon as they meet , move both pointers at same speed until they meet again 
+        slow = 0 ;
+        while(slow !=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        return slow;
     }
 };
