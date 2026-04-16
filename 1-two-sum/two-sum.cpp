725991008e7ae6189fa5858fce0edb9e92dda210
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-04-16 11:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (53.67%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
+        unordered_map<int,int>mp; //key -value pair
         for(int i = 0;i<nums.size();i++){
             int compliment = target-nums[i];
             if(mp.find(compliment)!=mp.end())  //if found 
                return {mp[compliment],i};
 
-               mp.insert({nums[i],i});
+               mp[nums[i]]=i;//insertion in hash map
         }
         return {};
         
