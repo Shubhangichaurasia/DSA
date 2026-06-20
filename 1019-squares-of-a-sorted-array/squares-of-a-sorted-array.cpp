f2ecfe11beea06264f0896a53bc3917e27419980
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-06-20 08:55:37 +0530

    Time: 3 ms (50.18%) | Memory: 31.1 MB (26.14%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
-          for(int i = 0 ; i<nums.size();i++){
-             nums[i] = nums[i]*nums[i];
-          }
-        //    for(int i = 0 ; i<nums.size();i++){
-             sort(nums.begin(),nums.end());
-        //   }
-       return nums;
+        int n = nums.size();
+        int j = 0 ; 
+        // finding boundaries 
+        while(j<n && nums[j]<0){
+            j++;
+        }
+        // j = first positive element 
+        int i = j-1;  //i = last negative element 
+        // applying merge sorted array approach '
+         vector<int>res;
+        while(i>=0 && j<n){
+            int left = nums[i]*nums[i];
+            int right = nums[j]*nums[j];
+           
+            if(left<right){
+                res.push_back(left);
+                i--;
+            }
+            else {
+              res.push_back(right);
+              j++;
+            }
+        }
+        while(i>=0){
+            res.push_back(nums[i]*nums[i]);
+            i--;
+        }
+        while(j<n){
+            res.push_back(nums[j]*nums[j]);
+            j++;
+        }
+return res;
     }
 };
