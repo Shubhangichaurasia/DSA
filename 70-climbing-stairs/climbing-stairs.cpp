b08ccb1a00ce3f62872952cd42d2528722c2e15a
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-08-16 23:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.69%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     int climbStairs(int n) {
-    //    //base case 
-    //    if(n==1 || n==2) return n;
+    //memory optimization + tabulation
+    if(n==1||n==2) return n ;
+
+    int prev1 = 2;//n-1
+    int prev2 = 1 ;//n-2
+
+    int result = prev1;
+    for(int i  = 3 ; i<=n;i++){
+        result = prev1+prev2;
+        prev2= prev1 ;
+        prev1 = result;
+
+            }
+   
+    
+    return result;
+    }
+};
+  /*
+  //base case 
+       if(n==1 || n==2) return n;
 
     //    //recursive function 
-    //    //2 choices climb 1 step at once or climb 2 steps at once
-    //     return climbStairs(n-1)//taking 1st step only
-    //      +climbStairs(n-2);// taking 2 steps at once
+    // 2 choices climb 1 step at once or climb 2 steps at once
+        return climbStairs(n-1)//taking 1st step only
+         +climbStairs(n-2);// taking 2 steps at once
+         
 
-    //tabulation dp
+
+         //tabulation dp
     
    if(n<=2) return n;
     vector<int>dp(n+1);
@@ -20,7 +41,4 @@ public:
        dp[i] = dp[i-1]+dp[i-2];
     return dp[n];
     }
-    
-    //memoization dp 
-    
-};
+*/
