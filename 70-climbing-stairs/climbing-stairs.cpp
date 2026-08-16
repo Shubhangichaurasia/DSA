1ccb79d380305810bcf311ad720816c37d98675e
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-08-16 23:25:21 +0530

    Time: 2 ms (5.03%) | Memory: 8.5 MB (54.04%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    //    //base case 
+    //    if(n==1 || n==2) return n;
+
+    //    //recursive function 
+    //    //2 choices climb 1 step at once or climb 2 steps at once
+    //     return climbStairs(n-1)//taking 1st step only
+    //      +climbStairs(n-2);// taking 2 steps at once
+   if(n<=2) return n;
+    vector<int>dp(n+1,-1);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3;i<=n ;i++)
+       dp[i] = dp[i-1]+dp[i-2];
+    return dp[n];
+    }
+};
