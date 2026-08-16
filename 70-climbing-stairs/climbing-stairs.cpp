3133c601c33a0fd138dff45bb21362898617c63c
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-08-16 23:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (39.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,13 +8,19 @@ public:
     //    //2 choices climb 1 step at once or climb 2 steps at once
     //     return climbStairs(n-1)//taking 1st step only
     //      +climbStairs(n-2);// taking 2 steps at once
+
+    //tabulation dp
+    
    if(n<=2) return n;
-    vector<int>dp(n+1,-1);
-    dp[0] = 1;
+    vector<int>dp(n+1);
+   // dp[0] = 1;
     dp[1] = 1;
     dp[2] = 2;
     for(int i = 3;i<=n ;i++)
        dp[i] = dp[i-1]+dp[i-2];
     return dp[n];
     }
+    
+    //memoization dp 
+    
 };
