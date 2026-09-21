607007a59a8a911d47c95078d7063bf3ce8861c9
Shubhangichaurasia <157537331+Shubhangichaurasia@users.noreply.github.com>
2026-09-22 00:15:13 +0530

    Time: 15 ms (11.27%) | Memory: 14.2 MB (76.60%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int>ans;
+        
+        for(int i = 0 ; i <nums.size();i++){
+            int k = 0 ; 
+            for(int j = 0 ; j <nums.size();j++){
+                if(j!=i && nums[j]<nums[i])
+                k++;
+               
+            }
+             ans.push_back(k);
+
+        }
+        return ans ;
+    }
+};
